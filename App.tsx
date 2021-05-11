4a752410d09e7a418e7d4ed3edb72f6a7dbe7076
Allen Goodman <allen.goodman@icloud.com>
2021-05-11 14:23:38 -0400

    metrics

--- App.tsx
+++ App.tsx
@@ -10,26 +10,50 @@ import * as Animatable from 'react-native-animatable';
 import {Colors, ListItem, Text} from 'react-native-ui-lib';
 
 type Assay = {
-  description: string;
   id: string;
   name: string;
+  description: string;
+  metric: string;
 }
+
+type Metric = {
+  id: string;
+  f: (image: tensorflow.Tensor3D) => number
+};
 
 const ASSAYS: Array<Assay> = [
   {
     id: "46d8394a-f094-41bb-b01d-f5cd23c8b165",
     description: "Nullam semper vitae libero suscipit ornare.",
-    name: "Integer viverra"
+    name: "Integer viverra",
+    metric: "21274555-28fa-4bb3-9be3-443e2e5dbb8d"
   },
   {
     id: "6009a0d9-9d75-43ce-8d92-de2da55fc3f7",
     description: "Curabitur velit odio, auctor quis eleifend ut, suscipit id odio.",
-    name: "Viverra"
+    name: "Viverra",
+    metric: "34c6c9a5-747d-420d-9700-ce61a22b030a"
   },
   {
     id: "41e2c106-c8e5-4f6a-aa7b-612e8dac233a",
     description: "Suspendisse mattis aliquam lectus.",
-    name: "Etiam viverra"
+    name: "Etiam viverra",
+    metric: "03114c27-bf26-44ff-879c-ef4be677d09e"
+  }
+]
+
+const METRICS: Array<Metric> = [
+  {
+    id: "21274555-28fa-4bb3-9be3-443e2e5dbb8d",
+    f: (image: tensorflow.Tensor3D) => 1.0,
+  },
+  {
+    id: "34c6c9a5-747d-420d-9700-ce61a22b030a",
+    f: (image: tensorflow.Tensor3D) => 1.0,
+  },
+  {
+    id: "03114c27-bf26-44ff-879c-ef4be677d09e",
+    f: (image: tensorflow.Tensor3D) => 1.0,
   }
 ]
 
@@ -162,6 +186,7 @@ const CameraScreen = () => {
 
   const [permissionGranted, setPermissionGranted] = useState<boolean | null>(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [score, setScore] = useState<number>();
 
   useEffect(() => {
     const effect = async () => {
@@ -223,4 +248,3 @@ export default () => {
     </NavigationContainer>
   );
 }
-

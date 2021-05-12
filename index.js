d1d46c634e3c2a1c88b59e86e90cb1073ef94950
Allen Goodman <allen.goodman@icloud.com>
2021-05-12 13:44:28 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import { App } from './src/components/App/App';
+import { App } from './src/components';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,

a3d3efe4aaeb6e3ce8a778ad72e9e32c1de59ff0
Allen Goodman <allen.goodman@icloud.com>
2021-05-13 17:19:31 -0400

    onPress

--- src/components/App/CameraScreen/ScoreView/CircleView/CircleView.tsx
+++ src/components/App/CameraScreen/ScoreView/CircleView/CircleView.tsx
@@ -2,11 +2,16 @@ import {View} from "react-native";
 import {CircleViewStyleSheet} from "./CircleViewStyleSheet";
 import {Text} from "react-native-ui-lib";
 import React from "react";
+import {TouchableOpacity} from "react-native-ui-lib/core";
 
 export const CircleView = ({score}: { score: number }) => {
+  const onPress = () => {}
+
   return (
-    <View style={CircleViewStyleSheet.view}>
-      <Text style={CircleViewStyleSheet.text}>{Math.floor(score * 100)}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <View style={CircleViewStyleSheet.view}>
+        <Text style={CircleViewStyleSheet.text}>{Math.floor(score * 100)}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
